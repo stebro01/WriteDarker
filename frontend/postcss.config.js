ff146ea0849394efab6997ed748c45fe2b6f3ad4
ste <ste@orb_ubuntu22_mbm.de>
2025-07-30 19:20:22 +0200

    chore: update Tailwind CSS integration and dependencies
    - Added Tailwind CSS and related plugins to package.json and package-lock.json.
    - Updated postcss.config.js to include Tailwind CSS.
    - Modified quasar.config.js to include 'tailwind.css' in the CSS array.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,8 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 import autoprefixer from 'autoprefixer'
+import tailwindcss from "@tailwindcss/postcss"; /** Add this  */
+
 // import rtlcss from 'postcss-rtlcss'
 
 export default {
@@ -18,6 +20,7 @@ export default {
         'last 4 iOS versions',
       ],
     }),
+    tailwindcss(),
 
     // https://github.com/elchininet/postcss-rtlcss
     // If you want to support RTL css, then
